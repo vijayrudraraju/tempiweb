55226d142a1729fc1ef69fcc1cb6f0b26940aeaa
Vijay Rudraraju <vijay326@gmail.com>
2011-11-01 16:53:34 -0400

    click to add node

--- js/main.js
+++ js/main.js
@@ -5,29 +5,24 @@
 
 // console.log is a function that google chrome uses to display debug information in its javascript console 
 
+// global processing object containing processing methods for drawing
 var gP;
 $(document).ready(function() {
         $(document).evently({
             _init: function() {
             },
             keypress: function(e) {
-                //e.preventDefault();
             },
             keydown: function(e) {
-                if(e.which=='9') {
-                    //e.preventDefault();
-                } else if(e.which=='13') {
-                    e.preventDefault();
-                }
             },
             keyup: function(e) {
-                if(e.which=='9') {
-                    //e.preventDefault();
-                } else if(e.which=='13') {
-                    e.preventDefault();
-                    $('#globalCanvas').trigger("enter");
-                }
-                console.log('keyup: '+e.which);
+            }
+        });
+
+        $('#nodeDiv').evently({
+            _init: function() {
+                $(this).data('idCounter',1);
+                $(this).data('nodeList',{});
             }
         });
 
@@ -36,43 +31,56 @@ $(document).ready(function() {
                 $(this).data('canvasWidth',640);
                 $(this).data('canvasHeight',640);
             
+                console.log('_init');
                 gP = new Processing($('#globalCanvas')[0],gP);
-                $(this).trigger('redraw');
+                $(this).trigger('requestdraw');
                 gP.noLoop();
             },
-            updatebackground: function() {
+            setupcanvas: function() {
             },
-            updategraph: function() {
-                console.log('updategraph triggered');
+            updatestates: function() {
+                console.log('updatestates triggered');
+                updateAllNodeMouseStates();
             },
-            redraw: function() {
+            draw: function() {
+                console.log('draw triggered');
+                // clear canvas
+                gP.background(0*16+11,0*16+9,0*16+11);
+                // redraw canvas
+                drawAllNodes();
+            },
+            requestdraw: function() {
+                console.log('requestdraw triggered');
                 gP.redraw();
-                console.log('redraw triggered');
             },
-            tab: function() {
-            },
-            enter: function() {
+            createnode: function() {
+                var myType = 'foo';
+                createNode(myType);
             }
         });
 });
 
 function gP(p) {
 	p.mouseMoved = function() {
-        $('#globalCanvas').trigger('redraw');
+        $('#globalCanvas').trigger('updatestates');
+        $('#globalCanvas').trigger('requestdraw');
 	};
 
 	p.mouseClicked = function() {
-        $('#globalCanvas').trigger('updategraph');
-        $('#globalCanvas').trigger('redraw');
+        $('#globalCanvas').trigger('createnode');
+        $('#globalCanvas').trigger('requestdraw');
 	};
-
+    // p.setup has to complete before gP is accesible
 	p.setup = function() {
-		console.log(p.PFont.list());
-
-		p.size($('#globalCanvas').data('canvasWidth'),$('#globalCanvas').data('canvasHeight'));
-		$('#globalCanvas').data('font',p.loadFont('sans-serif'));
+        console.log('available fonts:'+p.PFont.list());
+        p.size($('#globalCanvas').data('canvasWidth'),$('#globalCanvas').data('canvasHeight'));
+        $('#globalCanvas').data('font',p.loadFont('sans-serif'));
+        // initialize colors to none
+        p.noStroke();
+        p.noFill();
 	};
 
 	p.draw = function() {
+        $('#globalCanvas').trigger('draw');
 	};
 }
